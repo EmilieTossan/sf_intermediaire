8c455525ae99ae9d363fe82e0377c4e0f3e909ec
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-13 17:32:29 +0100

    Product, Category, Brand

---
+++ src/Controller/Admin/AdminCategoryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AdminCategoryController extends AbstractController
+{
+    /**
+     * @Route("admin/categories", name="admin_category_list")
+     */
+    public function AdminCategoryList(CategoryRepository $categoryRepository)
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('admin/categories.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * @Route("category/{id}", name="category_show")
+     */
+    public function AdminCategoryShow($id, CategoryRepository $categoryRepository)
+    {
+        $category = $categoryRepository->find($id);
+        return $this->render('admin/category.html.twig', ['category' => $category]);
+    }
+
+    /**
+     * @Route("admin/create/category", name="admin_create_category")
+     */
+    public function adminCreateCategory(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface
+    )
+    {
+        $category = new Category();
+        $categoryForm = $this->createForm(CategoryType::class, $category);
+        $categoryForm->handleRequest($request);
+
+        if($categoryForm->isSubmitted() && $categoryForm->isValid()){
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('admin_category_list');
+        }
+        return $this->render('admin/categoryform.html.twig', ['categoryForm' => $categoryForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/update/category/{id}", name="admin_update_category")
+     */
+    public function adminUpdateCategory(
+        $id,
+        CategoryRepository $categoryRepository,
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        SluggerInterface $sluggerInterface
+    ){
+        $category = $categoryRepository->find($id);
+        $categoryForm = $this->createForm(CategoryType::class, $category);
+        $categoryForm->handleRequest($request);
+
+        if ($categoryForm->isSubmitted() && $categoryForm->isValid()) {
+            /*$mediaFile = $categoryForm->get('media')->getData();
+            if ($mediaFile) {
+                $originalFilename = pathinfo($mediaFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $sluggerInterface->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $mediaFile->guessExtension();
+                $mediaFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+                $category->setMedia($newFilename);
+            }*/
+            $entityManagerInterface->persist($category);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('admin_category_list');
+        }
+        return $this->render('admin/categoryform.html.twig', ['categoryForm' => $categoryForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/delete/category/{id}", name="admin_delete_category")
+     */
+    public function adminDeleteCategory(
+        $id,
+        CategoryRepository $categoryRepository,
+        EntityManagerInterface $entityManagerInterface
+    ){
+        $category = $categoryRepository->find($id);
+        $entityManagerInterface->remove($category);
+        $entityManagerInterface->flush();
+        return $this->redirectToRoute("admin_category_list");
+    }
+}
+
---
+++ src/Controller/Admin/AdminProductController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminProductController extends AbstractController
+{
+    /**
+     * @Route("admin/products", name="admin_product_list")
+     */
+    public function AdminProductList(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+        return $this->render('admin/products.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("product/{id}", name="product_show")
+     */
+    public function AdminProductShow($id, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+        return $this->render('admin/product.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("admin/create/product", name="admin_create_product")
+     */
+    public function adminCreateProduct(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface
+    )
+    {
+        $product = new Product();
+        $productForm = $this->createForm(ProductType::class, $product);
+        $productForm->handleRequest($request);
+
+        if($productForm->isSubmitted() && $productForm->isValid()){
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('admin_product_list');
+        }
+        return $this->render('admin/productform.html.twig', ['productForm' => $productForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/update/product/{id}", name="admin_update_product")
+     */
+    public function adminUpdateProduct(
+        $id,
+        ProductRepository $productRepository,
+        Request $request,
+        EntityManagerInterface $entityManagerInterface
+    ){
+        $product = $productRepository->find($id);
+        $productForm = $this->createForm(ProductType::class, $product);
+        $productForm->handleRequest($request);
+
+        if($productForm->isSubmitted() && $productForm->isValid()){
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+            return $this->redirectToRoute('admin_product_list');
+        }
+        return $this->render('admin/productform.html.twig', ['productForm' => $productForm->createView()]);
+    }
+
+    /**
+     * @Route("admin/delete/product/{id}", name="admin_delete_product")
+     */
+    public function adminDeleteProduct(
+        $id,
+        ProductRepository $productRepository,
+        EntityManagerInterface $entityManagerInterface
+    ){
+        $product = $productRepository->find($id);
+        $entityManagerInterface->remove($product);
+        $entityManagerInterface->flush();
+        return $this->redirectToRoute("admin_product_list");
+    }
+}
+
