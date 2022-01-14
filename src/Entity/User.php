7054f9242e05fd963c5c362e03179abc21d86c39
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-14 16:38:57 +0100

    User date

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,6 +24,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private $password;
 
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $date;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -93,4 +96,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
 }
